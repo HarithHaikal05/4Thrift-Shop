ead74fcf125efc84b512b90230ff8c7229d7f7e1
HarithHaikal05 <haikalesham02@gmail.com>
2026-01-10 02:21:39 +0800

    Update ProductListServlet.java

--- src/main/java/com/thrift/controller/ProductListServlet.java
+++ src/main/java/com/thrift/controller/ProductListServlet.java
@@ -1,58 +1,53 @@
 package com.thrift.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.thrift.utils.DBConnection;
-import com.thrift.model.Product;
 
 @WebServlet("/ProductListServlet")
 public class ProductListServlet extends HttpServlet {
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        List<Product> productList = new ArrayList<>();
+        // 1. Tell the browser we are sending JSON text, not HTML
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
         
         try {
             Connection con = DBConnection.getConnection();
-            
-            // Get all available products
             String sql = "SELECT * FROM products";
-            
-            // OPTIONAL: Member 2 can add search features later like:
-            // String category = request.getParameter("category");
-            // if(category != null) sql += " WHERE category = '" + category + "'";
-            
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             
+            // 2. Manually build the JSON String (Since we can't use libraries like Gson)
+            StringBuilder json = new StringBuilder("[");
+            boolean first = true;
+            
             while (rs.next()) {
-                Product p = new Product();
-                p.setId(rs.getInt("id"));
-                p.setName(rs.getString("name"));
-                p.setDescription(rs.getString("description"));
-                p.setPrice(rs.getDouble("price"));
-                p.setSize(rs.getString("size"));
-                p.setCategory(rs.getString("category"));
-                p.setImageUrl(rs.getString("image_url"));
-                p.setStock(rs.getInt("stock"));
+                if (!first) json.append(",");
+                first = false;
                 
-                productList.add(p);
+                json.append("{");
+                json.append("\"id\":").append(rs.getInt("id")).append(",");
+                json.append("\"name\":\"").append(rs.getString("name")).append("\",");
+                json.append("\"price\":").append(rs.getDouble("price")).append(",");
+                json.append("\"image\":\"").append(rs.getString("image_url")).append("\"");
+                json.append("}");
             }
+            json.append("]");
             
-            // Send the list to the JSP
-            request.setAttribute("products", productList);
-            
-            // Forward to the Home Page (index.jsp)
-            request.getRequestDispatcher("index.jsp").forward(request, response);
+            // 3. Send the text to the browser
+            out.print(json.toString());
+            out.flush();
             
         } catch (Exception e) {
             e.printStackTrace();
